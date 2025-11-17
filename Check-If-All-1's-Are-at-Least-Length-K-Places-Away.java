4c494f1b11246f23802427cfce573581b89f7a5b
Priyanshu sharma <122525350+Priyanshucs22@users.noreply.github.com>
2025-11-17 22:46:51 +0530

    [1437] [Time Beats: 99.71%] [Memory Beats: 19.43%] - LeetPush

---
+++ Check-If-All-1's-Are-at-Least-Length-K-Places-Away.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int p = -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                if(p==-1){
+                    p=i;
+                }
+                else{
+                    if(i-p-1 < k){
+                        return false;
+                    }
+                }
+                p = i;
+            }
+        }
+        return true;
+    }
+}
