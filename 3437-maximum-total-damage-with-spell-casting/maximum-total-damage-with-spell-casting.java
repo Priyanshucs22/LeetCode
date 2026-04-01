aa498b9cab523c27b5bb0f800cda28548c4eba4d
Priyanshu sharma <122525350+Priyanshucs22@users.noreply.github.com>
2026-04-01 10:13:18 +0530

    Time: 765 ms (5.26%) | Memory: 198.7 MB (8.84%) - LeetSync

---
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long maximumTotalDamage(int[] power){
+        TreeMap<Integer , Long> dp = new TreeMap<>();
+        for(int P : power) dp.put(P , dp.getOrDefault(P , 0L) + P);
+        long maxi = 0 , last = 0;
+        for(int P : dp.keySet()){
+            Map.Entry<Integer , Long> it = dp.floorEntry(P - 3);
+            if(it != null) dp.put(P , dp.get(P) + it.getValue());
+            dp.put(P, last = Math.max(dp.get(P) , last));
+            maxi = Math.max(maxi , last);
+        }
+        return maxi;
+    }
+}
