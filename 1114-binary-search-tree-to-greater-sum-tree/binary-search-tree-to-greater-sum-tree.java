aa9c9e2a9c7587208b580c95e712dd576876edfe
Priyanshu sharma <122525350+Priyanshucs22@users.noreply.github.com>
2026-04-03 08:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (27.46%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    private void inOrder(TreeNode node){
+        if(node == null) return;
+        inOrder(node.right);
+        sum += node.val;
+        node.val = sum;
+        inOrder(node.left);
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        inOrder(root);
+        return root;
+    }
+}
